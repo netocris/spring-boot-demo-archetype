5e7ac110d6b158e2b2605e28a89623579ac2d1d6
Cristóvão Neto <cristovao.neto@informadb.pt>
2016-08-19 15:28:09 +0100

    first commit

---
+++ src/main/resources/archetype-resources/src/main/java/Application.java
@@ -0,0 +1,16 @@
+package ${groupId};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by netoc on 11/08/2016.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/resources/archetype-resources/src/test/java/ApplicationTest.java
@@ -0,0 +1,17 @@
+package ${groupId};
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by netoc on 11/08/2016.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
